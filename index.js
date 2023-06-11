ccd0e118ae04ef3c1d45d2f7590a6a29b881dc94
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2023-06-11 04:19:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 //EJERCICIO 1
 
-const changeColor = () => {
-    let cuadrado = document.getElementById("cuadrado");
-    let nameColor = document.getElementById("nombreDelColor")
+let boton = document.getElementById("boton")
+let cuadrado = document.getElementById("cuadrado");
+let nameColor = document.getElementById("nombreDelColor")
 
+const cambiarColor = () => {
+  if (cuadrado.classList.contains("bg-secondary")) {
+    cuadrado.classList.remove("bg-secondary")
+    cuadrado.classList.add("bg-primary")
+    nameColor.textContent = "CELESTE"
+  } 
+  else if (cuadrado.classList.contains("bg-primary")) {
+    cuadrado.classList.remove("bg-primary")
+    cuadrado.classList.add("bg-danger")
+    nameColor.textContent = "ROJO"
+  } 
+  else {
+    cuadrado.classList.remove("bg-danger")
+    cuadrado.classList.add("bg-secondary")
+    nameColor.textContent = "GRIS"
+  } 
+}
 
-    if (cuadrado.style.backgroundColor === "gray") {
-      cuadrado.style.backgroundColor = "rgb(40, 172, 172)";
-      nameColor.textContent = "CELESTE"
-    } 
-    else if (cuadrado.style.backgroundColor === "rgb(40, 172, 172)") {
-      cuadrado.style.backgroundColor = "red";
-      nameColor.textContent = "ROJO"
-    } 
-    else {
-      cuadrado.style.backgroundColor = "gray";
-      nameColor.textContent = "GRIS"
-    }
-
-  }
-
+boton.addEventListener("click", cambiarColor)
 
 // EJERCICIO 2
 
 let textoIngresado = document.getElementById("textoIngresado");
 let texto = document.getElementById("texto");
+let borrar = document.getElementById("borrar")
 
 const mostrarTexto = () => {
     texto.textContent = textoIngresado.value;
@@ -35,16 +39,20 @@ const borrarTexto = () => {
     texto.textContent = ""
 }
 
+textoIngresado.addEventListener("input", mostrarTexto)
+borrar.addEventListener("click", borrarTexto)
+
 
 //EJERCICIO 3
 
-const calcular = () => {
-    let peso = document.getElementById("peso");
-    let altura = document.getElementById("altura");
-    let resultado = document.getElementById("resultado");
-    
-    // Uso "parseFloat" para convertir los numeros enteros en flotantes
+let peso = document.getElementById("peso");
+let altura = document.getElementById("altura");
+let resultado = document.getElementById("resultado");
+let boton_calcular = document.getElementById("boton-calcular")
 
+const calcular = () => {
+
+    // Uso "parseFloat" para convertir los numeros enteros en flotantes
     peso = parseFloat(peso.value);
     altura = parseFloat(altura.value) / 100; // Convertir altura a metros
     
@@ -52,18 +60,25 @@ const calcular = () => {
     resultado.value = imc.toFixed(1); // "toFixed(1)" es para mostrar el resultado con 1 decimal
   }
 
+boton_calcular.addEventListener("click", calcular)
+
 
 //EJERCICIO 4
 
+
 const convertir = () => {
 
-  let usd = document.getElementById("usd")
-  let pesoArg = document.getElementById("pesoArg")
-  let valorConvertido = document.getElementById("valorConvertido")
+let usd = parseFloat(document.getElementById("usd").value)
+let pesoArg = parseFloat(document.getElementById("pesoArg").value)
+let valorConvertido = document.getElementById("valorConvertido")  
 
-  usd = parseFloat(usd.value)
-  pesoArg = parseFloat(pesoArg.value)
-  
-  let convertedValue = usd * pesoArg
-  valorConvertido.value = convertedValue.toFixed(2) 
+  if (isNaN(usd) || isNaN(pesoArg)) {
+    valorConvertido.value = "";
+  } else {
+    const convertedValue = usd * pesoArg;
+    valorConvertido.value = convertedValue.toFixed(2);
+  }
 }
+
+usd.addEventListener("input",convertir)
+pesoArg.addEventListener("input",convertir)
